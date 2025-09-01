7c5db1bdd77619edb826d1e016f4455b5b3f729f
vahahahart <vahryshev2002@gmail.com>
2025-09-02 01:21:31 +0700

    Fix imports in test_optimizer.py

--- tests/test_optimizer.py
+++ tests/test_optimizer.py
@@ -7,7 +7,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import numpy as np
 import pytest
 from lmfit import Parameters
-from lib import Optimizer, ndpvoigt
+from lib.optimization import Optimizer
+from lib.funcs import ndpvoigt
 
 
 @pytest.fixture
